6685170be81fb04c75cc7c3834af4c4f26b458a4
Gal Tashma <galtashma@gmail.com>
2019-05-13 16:59:27 +0800

    Removed unused code to switch between languages

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,8 @@
 <script>
 	import SearchBar from './components/searchbar.svelte';
-
-	let name = 'world';
-	let MODE_ENG_CHN = 0;
-	let MODE_CHN_ENG = 1;
-	let mode = MODE_CHN_ENG;
-	let toTranslate = "";
 	let wikiEntry = getDummyWikipediaEntry();
-	
-	var log="...";
-	
-	let lang_from, lang_to = "";
-	updateModeString();
-	
-	function updateModeString() {
-		if (mode == MODE_CHN_ENG) {
-			lang_from = "CHN";
-			lang_to = "ENG";
-			return;
-		}
-		lang_from = "ENG";
-		lang_to = "CHN";
-	}
-	
-	function toggleMode() {
-		mode = (mode+1) % 2;
-		updateModeString();
-	}
-	
+	let toTranslate = "";
+
 	function onTranslateChange() {
 		wikiEntry = getWikipediaEntry(toTranslate);
 	}
@@ -38,7 +13,6 @@
 	
 	async function getWikipediaEntry(entry) {
 		const url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&prop=info|langlinks&lllang=zh&titles=`+entry;
-		log = url;
 		const res = await fetch(url);
 		const j = await res.json();
 		if (res.ok) {
@@ -49,8 +23,8 @@
 		}
 		throw new Error("Could'nt fetch from wikipedia");
 	}
-	
 </script>
+
 <div>
 	<h1>ZenMeShuo?!</h1>
 	<SearchBar on:change={onTranslateChange} bind:value={toTranslate}/>
