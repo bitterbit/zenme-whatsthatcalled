66981b924c55a450d588433bbc3966fcec196ef5
Gal Tashma <galtashma@gmail.com>
2019-05-13 17:12:31 +0800

    added hint/default value to search bar

--- src/components/searchbar.svelte
+++ src/components/searchbar.svelte
@@ -1,5 +1,19 @@
 <script>
     export let value;
+    let defaultValue = "McDonald's";
+    let hasDefault = true;
+    value = defaultValue;
+
+    function onFocus(){
+        value = "";
+        hasDefault = false;
+    }
 </script>
+<style>
+    .default_value {
+        color: #ccc;
+    }
+</style>
 <span>Word: </span>
-<input a=1 on:change bind:value={value} type=text/>
+
+<input class="{hasDefault? 'default_value' : ''}" on:focus={onFocus} on:change bind:value={value} type=text/>
